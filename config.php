3ca8225795a0cf9f97d45e23b705526d1880fac5
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2019-01-19 16:36:16 +0200

    add config file

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+    define('LIBRARY', 'lib/');
+
+    define('ROOT_DIR', dirname(__FILE__));
+
+    define('BASE_URL', 'http://localhost/todomvc/');
+
+    define('APP_NAME', 'TodoMVC');
+
+    define('DB_VENDOR', 'mysql');
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'todo_db');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', '');
